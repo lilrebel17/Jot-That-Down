1d68cab13f6c9f9d85c655f1324ce988a896d3e2
Zachary Farmer <72500445+lilrebel17@users.noreply.github.com>
2022-03-29 09:57:14 -0400

    One note populating properly
    I managed to make it store a couple of variables in local storage and have the browser pull it to put it on screen. NoteID isnt working properly at the moment so it will only show one note.

--- notesubmit.js
+++ notesubmit.js
@@ -1,11 +1,52 @@
+let NoteID = 0
+
 let submitbutton = document.querySelector('.submit-note-button')
+let NoteListContainer = document.querySelector('.notes-list-container')
 
-submitbutton.addEventListener('click', () => {
-    let content = document.querySelector('.note-content')
-    let title = document.querySelector('.note-title')
-    let dueDate = document.querySelector('.note-due-date')
+let content = document.querySelector('.note-content')
+let title = document.querySelector('.note-title')
+let dueDate = document.querySelector('.note-due-date')
 
-    console.log(content.value)
-    console.log(title.value)
-    console.log(dueDate)
+window.onload = () =>{
+    for(let i=0; i < localStorage.length; i++) {
+        let NewNoteContainer = document.createElement('li')
+        let NewNoteTitle = document.createElement('h1')
+        let NewNoteDueDate = document.createElement('p')
+        let NewNoteContent = document.createElement('p')
+
+        NewNoteContainer.id = localStorage.getItem('JTD NoteID' + i)
+        NewNoteTitle.innerHTML = localStorage.getItem('JTD NoteTitle' + i)
+        NewNoteDueDate.innerHTML = localStorage.getItem('JTD NoteDueDate' + i)
+        NewNoteContent.innerHTML = localStorage.getItem('JTD NoteContent' + i)
+
+        NewNoteContainer.append(NewNoteTitle, NewNoteDueDate,NewNoteContent)
+        NoteListContainer.append(NewNoteContainer)
+        
+
+        console.log(NoteID)
+        if(localStorage.getItem = null) {
+        }
+    }
+}
+
+submitbutton.addEventListener('click', () => { 
+    let NewNoteContainer = document.createElement('li')
+    let NewNoteTitle = document.createElement('h1')
+    let NewNoteDueDate = document.createElement('p')
+    let NewNoteContent = document.createElement('p')
+
+    NewNoteContainer.id = NoteID
+    NewNoteTitle.innerHTML = title.value
+    NewNoteDueDate.innerHTML = dueDate.value
+    NewNoteContent.innerHTML = content.value
+
+    NewNoteContainer.append(NewNoteTitle, NewNoteDueDate,NewNoteContent)
+    NoteListContainer.append(NewNoteContainer)
+
+    localStorage.setItem("JTD NoteID" + NoteID, NoteID)
+    localStorage.setItem('JTD NoteTitle' + NoteID, title.value)
+    localStorage.setItem('JTD NoteDueDate' + NoteID, dueDate.value)
+    localStorage.setItem('JTD NoteContent' + NoteID, content.value)
+
+    NoteID += 1
 })
