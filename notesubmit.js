322fd98855a97a06228849f6e18da603e0374c6e
Zachary Farmer <72500445+lilrebel17@users.noreply.github.com>
2022-03-29 11:59:18 -0400

    Note submittal, storage, and NoteIDs working
    Had to refactor the way I was storing the information for the notes.
    
    It was just storing all 4 variables in 4 different strings. Now instead Its convering the localStorage item into a JSON string. Then on window load its parsing the string to make it into an array again. And displaying properly.
    
    NoteIDs just needed the proper operators assigned

--- notesubmit.js
+++ notesubmit.js
@@ -1,11 +1,10 @@
-let NoteID = 0
-
 let submitbutton = document.querySelector('.submit-note-button')
 let NoteListContainer = document.querySelector('.notes-list-container')
 
 let content = document.querySelector('.note-content')
 let title = document.querySelector('.note-title')
 let dueDate = document.querySelector('.note-due-date')
+let NoteID = 0
 
 window.onload = () =>{
     for(let i=0; i < localStorage.length; i++) {
@@ -13,19 +12,18 @@ window.onload = () =>{
         let NewNoteTitle = document.createElement('h1')
         let NewNoteDueDate = document.createElement('p')
         let NewNoteContent = document.createElement('p')
+        let OldNote = JSON.parse(localStorage.getItem('Note' + i))
 
-        NewNoteContainer.id = localStorage.getItem('JTD NoteID' + i)
-        NewNoteTitle.innerHTML = localStorage.getItem('JTD NoteTitle' + i)
-        NewNoteDueDate.innerHTML = localStorage.getItem('JTD NoteDueDate' + i)
-        NewNoteContent.innerHTML = localStorage.getItem('JTD NoteContent' + i)
+        NewNoteContainer.id = OldNote[0]
+        NewNoteTitle.append(OldNote[1])
+        NewNoteDueDate.append(OldNote[2])
+        NewNoteContent.append(OldNote[3])
 
         NewNoteContainer.append(NewNoteTitle, NewNoteDueDate,NewNoteContent)
         NoteListContainer.append(NewNoteContainer)
-        
 
-        console.log(NoteID)
-        if(localStorage.getItem = null) {
-        }
+        NoteID = OldNote[0] + 1
+        console.log(OldNote)
     }
 }
 
@@ -43,10 +41,8 @@ submitbutton.addEventListener('click', () => {
     NewNoteContainer.append(NewNoteTitle, NewNoteDueDate,NewNoteContent)
     NoteListContainer.append(NewNoteContainer)
 
-    localStorage.setItem("JTD NoteID" + NoteID, NoteID)
-    localStorage.setItem('JTD NoteTitle' + NoteID, title.value)
-    localStorage.setItem('JTD NoteDueDate' + NoteID, dueDate.value)
-    localStorage.setItem('JTD NoteContent' + NoteID, content.value)
+    let StorageArray = [NoteID,title.value,dueDate.value,content.value]
+    localStorage.setItem('Note' + NoteID, JSON.stringify(StorageArray))
 
     NoteID += 1
 })
