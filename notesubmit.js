dece9daa17e2151f14470fbb9f9f4fa17046f2fb
Zachary Farmer <72500445+lilrebel17@users.noreply.github.com>
2022-03-31 08:58:57 -0400

    Selection,Notation, and Debug Styling.
    # Added a lot of notation on how things work and why
    
    #Got the selection working, now you can click anywhere in the list and it will select and update an array to use as a "Currently Selected div"
    
    #Added some styling just for debugging purposes.

--- notesubmit.js
+++ notesubmit.js
@@ -1,48 +1,89 @@
+//Variables for the Containers & HTML elements we are manipulating
 let submitbutton = document.querySelector('.submit-note-button')
 let NoteListContainer = document.querySelector('.notes-list-container')
+let editor = document.querySelector('.editor')
 
+//Variables for the input boxes
 let content = document.querySelector('.note-content')
 let title = document.querySelector('.note-title')
 let dueDate = document.querySelector('.note-due-date')
+
+//NoteID helps being able to get and pull specific notes from local storage
 let NoteID = 0
 
+//SelectedArray is to help keep up with your current selection
+let SelectedArray = []
+
+//Loads older notes when the window is loaded
 window.onload = () =>{
     for(let i=0; i < localStorage.length; i++) {
+        //Creating local variables for older notes
         let NewNoteContainer = document.createElement('li')
         let NewNoteTitle = document.createElement('h1')
         let NewNoteDueDate = document.createElement('p')
         let NewNoteContent = document.createElement('p')
+        let RemoveButton = document.createElement('button')
+
+        //All notes are stored as JSON String but are originally array.
+        //We have to parse the old notes so it converts back into
+        //An array we can use again
         let OldNote = JSON.parse(localStorage.getItem('Note' + i))
 
+        //Since its an array again, we can just use OldNote
+        //To store those old array items, and append them
+        //To match the note structure.
         NewNoteContainer.id = OldNote[0]
+        NewNoteContainer.className = 'notes-list-item' // Creates a class to select all notes
         NewNoteTitle.append(OldNote[1])
         NewNoteDueDate.append(OldNote[2])
         NewNoteContent.append(OldNote[3])
 
+        //This appends the newly created elements to the container and then
+        //Appends the container to its div "note-list-container"
         NewNoteContainer.append(NewNoteTitle, NewNoteDueDate,NewNoteContent)
         NoteListContainer.append(NewNoteContainer)
 
+        //This takes our NoteID, which is a global variable.
+        //Makes it equal to OldNote[0] which is OldNotes NoteID
+        //Then Adds 1, to go 1 above the previous NoteID
         NoteID = OldNote[0] + 1
-        console.log(OldNote)
     }
 }
 
-submitbutton.addEventListener('click', () => { 
-    let NewNoteContainer = document.createElement('li')
-    let NewNoteTitle = document.createElement('h1')
-    let NewNoteDueDate = document.createElement('p')
-    let NewNoteContent = document.createElement('p')
+//Creates Note and stores the data in LocalStorage when submit button is pressed
+submitbutton.addEventListener('click', () => {
+        //Creating local variables for new notes
+        let NewNoteContainer = document.createElement('li')
+        let NewNoteTitle = document.createElement('h1')
+        let NewNoteDueDate = document.createElement('p')
+        let NewNoteContent = document.createElement('p')
+        let RemoveButton = document.createElement('button')
 
-    NewNoteContainer.id = NoteID
-    NewNoteTitle.innerHTML = title.value
-    NewNoteDueDate.innerHTML = dueDate.value
-    NewNoteContent.innerHTML = content.value
+        //Adds values of the input boxes to the newley created Elements
+        NewNoteContainer.id = NoteID
+        NewNoteContainer.className = 'notes-list-item'
+        NewNoteTitle.innerHTML = title.value
+        NewNoteDueDate.innerHTML = dueDate.value
+        NewNoteContent.innerHTML = content.value
 
-    NewNoteContainer.append(NewNoteTitle, NewNoteDueDate,NewNoteContent)
-    NoteListContainer.append(NewNoteContainer)
+        //This appends the newly created elements to the container and then
+        //Appends the container to its div "note-list-container"
+        NewNoteContainer.append(NewNoteTitle, NewNoteDueDate,NewNoteContent)
+        NoteListContainer.append(NewNoteContainer)
 
-    let StorageArray = [NoteID,title.value,dueDate.value,content.value]
-    localStorage.setItem('Note' + NoteID, JSON.stringify(StorageArray))
+        //Creates an array, makes it a JSON string. Then stores it in localStorage
+        let StorageArray = [NoteID,title.value,dueDate.value,content.value]
+        localStorage.setItem('Note' + NoteID, JSON.stringify(StorageArray))
 
-    NoteID += 1
+        NoteID += 1
+})
+
+NoteListContainer.addEventListener('click', (a) => {
+        if(a.target.parentNode.className == 'notes-list-item') {
+            SelectedArray.push(a.target.parentNode.id)
+        }  
+        else if(a.target.className == 'notes-list-item') {
+            SelectedArray.push(a.target.id)
+        }
+    console.log(SelectedArray)
 })
