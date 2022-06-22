b490150f26ad3a1febe7cc8e4699022dc85128ab
Zachary Farmer <72500445+lilrebel17@users.noreply.github.com>
2022-06-22 07:19:23 -0400

    Updated Notation in Main.js
    Added actual notation to main.js

--- main.js
+++ main.js
@@ -27,12 +27,19 @@ let deleteYesButton = document.querySelector('.del-yes-btn')
 let deleteNoButton = document.querySelector('.del-no-btn')
 
 //Global Variable to keep track of your selection
+//SelectionArray[1] is the previously selected note
+//SelectionArray[0] is the currently selected not
 let SelectionArray = []
 
 
-//Loads older notes when the window is loaded
+//All notes are stored as an array, then parsed into localstorage under the same local storage item "Notes"
+//This onload function just checks for local storage items, and writes them to the left side of the screen if found
 window.onload =  () => {
+    //StoredNotes is used a local variable name a lot to help readability, if you see it in any function
+    //Its always parsing my local storage function, to be writen to or read
     let StoredNotes = JSON.parse(localStorage.getItem('Notes'))
+    //The loop is to be able to iterate through the StoredNotes array
+    //This way we dont miss one.
     for(i=0;i < StoredNotes.length; i++) {
     //Creating local variables for older notes
     let NoteContainer = document.createElement('li')
@@ -40,11 +47,14 @@ window.onload =  () => {
     let NoteDueDate = document.createElement('p')
     let NoteContent = document.createElement('p')
 
+    //Updating the notes container with the correct class name, ID and & note contents
     NoteContainer.className = 'notes-li'
     NoteContainer.id = i
-    NoteTitle.append(StoredNotes[i][0])
-    NoteDueDate.append(StoredNotes[i][1])
-    NoteContent.append(StoredNotes[i][2])
+    //Notes are always accessed this way, with the local variable StoredNotes
+    //[i] just refers whichever step of the loop its on
+    NoteTitle.append(StoredNotes[i][0])//[0] is always note name
+    NoteDueDate.append(StoredNotes[i][1])//[1] is always note date
+    NoteContent.append(StoredNotes[i][2])//[2] is always note content
 
     //This appends the newly created elements to the container and then
     //Appends the container to its div "note-list-container"
@@ -60,9 +70,14 @@ window.onload =  () => {
 }
 
 //****** SAVE FUNCTION ******//
+//No matter where the function is called from always pass NoteArray as a param
+//NoteArray is always an array with the following syntax:
+//[Title,DueDate,Content]
 function SaveNote(NoteArray) {
     //Check to make sure our localstorage item exists, creates it if not.
     if(localStorage.getItem('Notes') == undefined) {
+        //The item we use for everything is 'Notes'
+        //Its just an array, with every note as its own subarray
         let NotesStorageArray = []
         console.log("Nothing in local storage, creating new array")
         console.log(' ')
@@ -77,8 +92,9 @@ function SaveNote(NoteArray) {
         Storage.push(NoteArray)
         localStorage.setItem('Notes',JSON.stringify(Storage))
     }
-    //Saves previously selected note, as long as there is one. and we dont send
-    //a new array to SaveNote()
+    //Saves previously selected note, as long as there is one.
+    //Checks for a previously selected note
+    //And makes sure the previous selection & current selection arent the same
     else if(document.getElementById(SelectionArray[1]) != null && SelectionArray[0] != SelectionArray[1]) {
         let Storage = JSON.parse(localStorage.getItem('Notes'))
         let PreviousSelection = document.getElementById(SelectionArray[1])
@@ -92,6 +108,7 @@ function SaveNote(NoteArray) {
         localStorage.setItem('Notes',JSON.stringify(Storage))
     }
     //Saves currently selected note, as long as there is one
+    //Checks for a current selection, and makes sure we passed a param
     else if(SelectionArray[0] != undefined && NoteArray != undefined) {
         let Storage = JSON.parse(localStorage.getItem('Notes'))
         let CurrentSelection = document.getElementById(SelectionArray[0])
@@ -103,6 +120,7 @@ function SaveNote(NoteArray) {
         ]
         localStorage.setItem('Notes',JSON.stringify(Storage))
     }
+    //This shows & Hides the "Note Saved" banner up top
     let SaveBanner = document.querySelector('.save-alrt')
     SaveBanner.classList.remove('hidden')
     setTimeout(() => {
@@ -110,9 +128,13 @@ function SaveNote(NoteArray) {
     },2000)
 }
 
+//***** DELETE FUNCTION  *****/
+//Really simple, if we have a note selected(SelectionArray[0] != undefined)
+//Then we parse our storage array
+//We take our current selection, which is an ID based on its position in
+//Our local storage array. We then splice out that ID
+//And set the item again, stringify'ing the array so it moves to a JSON item
 function DeleteNote() {
-    //Checks for a current selection, if it has one, its removed from storage
-    //Also updates localstorage after deletion
     if(SelectionArray[0] != undefined){
         let Storage = JSON.parse(localStorage.getItem('Notes'))
         Storage.splice(SelectionArray[0],1)
@@ -127,6 +149,7 @@ function DeleteNote() {
 NoteListContainer.addEventListener('click', (a) => {
     //If you click on NoteListContainer or its parent,
     //It is added to the editor & the note list is updated
+    //Also updates SelectionArray
         if(a.target.parentNode.className == 'notes-li') {
             AddToEditor(a.target.parentNode.id)
             UpdateNoteList(a.target.parentNode.id)
@@ -175,6 +198,10 @@ function UpdateNoteList(id) {
 }
 
 function UpdateSelectionArray(currentid) {
+    //We unshift the currentid(which is whatever just got selected) so it stays
+    //At position 0
+    //If the Array is over 2 elements in length. We pop the array, so its never
+    //more than two elemets, a currentid and previousid
     SelectionArray.unshift(currentid)
     if(SelectionArray.length > 2) {
         SelectionArray.pop()
@@ -245,23 +272,36 @@ savebutton.addEventListener('click', () => {
 })
 
 deleteYesButton.addEventListener('click', () => {
+    //1.) Take CurrentNote, which is stored as SelectionArray[0](a global variable)
+    //and just removes its with 
+    //2.) We then have to shift all element IDs to makeup for the removed ID so
+    //we dont have any gaps. We do this with ShiftElementIds()
+    //3.) The editor needs to be reset so its blank
+    //4.) Then delete the note from storage with DeleteNote()
+    //5.) Finally set the current selection to undefined so we can go from a delete
+    //state to creating a new note
     let CurrentNote = document.getElementById(SelectionArray[0])
     let editor = document.querySelector('.edt-form')
 
-    deleteAlert.classList.add('hidden')
-    ShiftElementIDs(SelectionArray[0])
-    CurrentNote.remove()
-    editor.reset()
-    DeleteNote()    
-    SelectionArray[0] = undefined
+    deleteAlert.classList.add('hidden')//Hides the alert
+    ShiftElementIDs(SelectionArray[0])//Shifts all noteIDS to make up for deleted ID
+    CurrentNote.remove()//Removes the notes
+    editor.reset()//Resets the editors so its no longer in editor
+    DeleteNote()//Deletes note from storage array
+    SelectionArray[0] = undefined//Set current selection to undefined
 })
 
 deleteNoButton.addEventListener('click',() => {
     deleteAlert.classList.add('hidden')
 })
 
+//Only called when you hit yes after hitting the delete button
 function ShiftElementIDs(oldid) {
-    oldid = parseInt(oldid)
+    //oldid is SelectionArray[0] or the selection that just got deleted
+    oldid = parseInt(oldid) //Its a string when we pass it, so we parseInt so its an int
+    //We check to make sure oldid, doesnt equal the same number of child elements
+    //childElementCount is the same as children.length
+    //Check for this because we dont have to shift all IDs if its the last one
     if(oldid != NoteListContainer.childElementCount){
         for(let i=oldid;i < NoteListContainer.childElementCount; i++) {
             let div = document.getElementById(i)
@@ -273,14 +313,15 @@ function ShiftElementIDs(oldid) {
     }
 }
 
-
+//Reveals the "Yes" and "No" banner for deletion
 deletenotebutton.addEventListener('click', () => {
     if(SelectionArray[0] != null) {
         deleteAlert.classList.remove('hidden')
     }
 })
 
-
+//If you have something selected, clears the editor & removes whatever you had 
+//selected from the selection array
 EditorCreateButton.addEventListener('click', () => {
     if(SelectionArray[0] != undefined) {
         SelectionArray.splice(0,SelectionArray.length)
